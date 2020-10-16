f2d5f39f06cffa21a18f60725fddcd047c9ae960
Javier Ortiz Perez-Jaraiz <javier.ortiz@edu.uah.es>
2020-10-16 21:09:50 +0200

    intento de configuración servo

--- main.c
+++ main.c
@@ -4,9 +4,13 @@
 
 int main(){
 	
-	int i;
-	i = 0;
-	
-	
+
+
+	///// Comenzamos con la configuración de PWM 1.1
+	//// PCONP (Alimentación) por defecto a 1
+	//// Clock, no hace falta cambiar la configuración.
+	//   Pinsel:
+	LPC_PINCON->PINSEL3|= (1<<9);
+
 	
 }
